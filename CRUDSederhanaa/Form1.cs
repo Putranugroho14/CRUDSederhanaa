88d86ca38acdc260b0242b6680f150a1f889d50b
Putranugroho14 <putranugroho61@gmail.com>
2025-03-21 14:31:17 +0700

    menambahkan fungsi btnRefresh_Click untuk memperbarui data dan menampilkan jumlah kolom serta baris di DataGridView

--- CRUDSederhanaa/Form1.cs
+++ CRUDSederhanaa/Form1.cs
@@ -156,4 +156,13 @@ namespace CRUDSederhanaa
         }
 
 
-       
+        private void btnRefresh_Click_1(object sender, EventArgs e)
+        {
+            LoadData();
+
+            // Debugging: Cek jumlah kolom dan baris
+            MessageBox.Show($"Jumlah Kolom: {dgvMahasiswa.ColumnCount}\nJumlah Baris: {dgvMahasiswa.RowCount}",
+                            "Debugging DataGridView", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        
